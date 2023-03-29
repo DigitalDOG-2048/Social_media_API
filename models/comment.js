b73d5a08d0ca7a8e96fe6c072aaea84023aa8121
Longhui Huang <huangl43@coventry.ac.uk>
2023-03-29 22:22:26 +0000

    update likes router

--- models/comment.js
+++ models/comment.js
@@ -9,13 +9,17 @@ exports.getComment = async function getComment(ctx) {
 }
 
 exports.addComment = async function addComment(ctx) {
-    const body = ctx.request.body
+    const postId = ctx.params.postId;
+    const userId = ctx.params.userId;
+    Object.assign(ctx.request.body, {postId, userId})
+    const body = ctx.request.body;
+    console.log(body);
     const query = "INSERT INTO comments SET ?"
     const result = await db.sql_query(query, body)
     ctx.status = 200;
     ctx.body = {
         ID: result.insertId,
-        Message: `You have commnet on post ${body.postID}`,
+        Message: `You have commnet on post ${postId}`,
         Comment: body.comment,
     }
 }
@@ -28,5 +32,25 @@ exports.delComment = async function delComment(ctx) {
     ctx.body = {
         id: id,
         Message: 'comment has been deleted',
+    }
+}
+
+exports.updateComment = async function updateComment(ctx) {
+    const id = ctx.params.commentId;
+    const body = ctx.request.body;
+    const query = "UPDATE comments SET? WHERE id = ?"
+    console.log(body);
+    try{
+        await db.sql_query(query, [body, id]);
+        ctx.status = 200;
+        ctx.body = {
+            Message: `You have update the comment with comment id:${id}`,
+            newComment: body
+        }
+    }catch(error){
+        ctx.status = 500;
+        ctx.body = {
+            Message: `can not update the comment with comment id:${id}`
+        }
     }
 }
